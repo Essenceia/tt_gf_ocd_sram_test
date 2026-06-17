bcd3707215009ec468c03d0cc1870de9843962c4
Julia Desmazes - ws <julia.desmazes@gmail.com>
2026-06-16 21:24:09 -0500

    fix enable

---
+++ src/tt_um_essen.v
@@ -0,0 +1,60 @@
+/*
+Copyright (c) 2026 Julia Desmazes 
+
+This code was written by a human, authorization is explicitly not 
+granted to use it to train any model. 
+*/
+
+`default_nettype none
+
+module tt_um_essen (
+	input  wire [7:0] ui_in,    
+    output wire [7:0] uo_out,   
+    input  wire [7:0] uio_in,   
+    output wire [7:0] uio_out,  
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+assign uio_out = 8'd0;
+assign uio_oe  = 8'd0;
+
+// place sram
+reg  [7:0] mem_addr_q; 
+reg  [7:0] mem_d_q;
+reg        wen_q;
+
+wire [7:0] mem_q; 
+reg  [7:0] mem_d1_q;
+
+wire ui_in_unused; 
+assign ui_in_unused = ui_in[7];
+
+always @(posedge clk) begin
+	if (~rst_n) begin
+		mem_addr_q <= 8'd0;
+		mem_d_q    <= 8'd0;
+		mem_d1_q   <= 8'd0;
+		wen_q      <= 8'd0;
+	end else begin
+		mem_addr_q <= {1'b0, ui_in[6:0]};
+		mem_d_q    <= uio_in;
+		mem_d1_q   <= mem_q;
+		wen_q      <= ui_in[7];
+	end
+end
+assign uo_out = mem_d1_q;
+
+gf180mcu_ocd_ip_sram__sram256x8m8wm1 m_sram(
+	.CLK(clk), 
+	.CEN(~ena), // shut down sram in case this is ever included in a shuttle without power gatting
+	.GWEN(~rst_n), // set all entries to 0 on rst
+	.WEN({8{wen_q}}),
+	.A(mem_addr_q),
+	.D(mem_d_q),
+	.Q(mem_q)
+);
+
+endmodule
