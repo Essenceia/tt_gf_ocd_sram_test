8699a51cef5de631ba0bc73fd5ea2fa4de088047
Julia Desmazes - ws <julia.desmazes@gmail.com>
2026-06-17 14:00:05 -0500

    fixing sram connections, resizing to use 64x8 sram and 1x1 tile

--- src/tt_um_essen.v
+++ src/tt_um_essen.v
@@ -7,7 +7,11 @@ granted to use it to train any model.
 
 `default_nettype none
 
-module tt_um_essen (
+module tt_um_essen #(
+	localparam SRAM_LINE_CNT = 64,
+	localparam SRAM_ADDR_W = $clog2(SRAM_LINE_CNT),
+	localparam SRAM_DATA_W = 8
+)(
 	input  wire [7:0] ui_in,    
     output wire [7:0] uo_out,   
     input  wire [7:0] uio_in,   
@@ -22,24 +26,25 @@ assign uio_out = 8'd0;
 assign uio_oe  = 8'd0;
 
 // place sram
-reg  [7:0] mem_addr_q; 
-reg  [7:0] mem_d_q;
-reg        wen_q;
+reg  [SRAM_ADDR_W-1:0] mem_addr_q; 
+reg  [SRAM_DATA_W-1:0] mem_d_q;
+wire [SRAM_DATA_W-1:0] mem_q; 
+reg  [SRAM_DATA_W-1:0] mem_d1_q;
+reg                    wen_q;
+reg                    wen_q;
 
-wire [7:0] mem_q; 
-reg  [7:0] mem_d1_q;
 
-wire ui_in_unused; 
-assign ui_in_unused = ui_in[7];
+wire   ui_in_unused; 
+assign ui_in_unused = ui_in[6];
 
 always @(posedge clk) begin
 	if (~rst_n) begin
-		mem_addr_q <= 8'd0;
-		mem_d_q    <= 8'd0;
-		mem_d1_q   <= 8'd0;
-		wen_q      <= 8'd0;
+		mem_addr_q <= {SRAM_ADDR_W{1'b0}};
+		mem_d_q    <= {SRAM_DATA_W{1'b0}};
+		mem_d1_q   <= {SRAM_DATA_W{1'b0}};
+		wen_q      <= 1'b0;  
 	end else begin
-		mem_addr_q <= {1'b0, ui_in[6:0]};
+		mem_addr_q <= ui_in[5:0];
 		mem_d_q    <= uio_in;
 		mem_d1_q   <= mem_q;
 		wen_q      <= ui_in[7];
@@ -47,11 +52,11 @@ always @(posedge clk) begin
 end
 assign uo_out = mem_d1_q;
 
-gf180mcu_ocd_ip_sram__sram256x8m8wm1 m_sram(
+gf180mcu_ocd_ip_sram__sram64x8m8wm1 m_sram(
 	.CLK(clk), 
 	.CEN(~ena), // shut down sram in case this is ever included in a shuttle without power gatting
-	.GWEN(~rst_n), // set all entries to 0 on rst
-	.WEN({8{wen_q}}),
+	.GWEN(wen_q),
+	.WEN({SRAM_DATA_W{1'b0}}),
 	.A(mem_addr_q),
 	.D(mem_d_q),
 	.Q(mem_q)
